e15158c5c31b78b5d75d0c6cf22a0b525456bb75
Nikhilsarotri <nikhilc016@gmail.com>
2025-05-07 10:09:16 +0530

    Update candidateRoutes.js

--- server/Routes/candidateRoutes.js
+++ server/Routes/candidateRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 // Base route /api/candidates
 router.post('/', userAuth,upload.single('resume'), createCandidate);
-router.get('/', userAuth,getCandidates);
+router.get('/',getCandidates);
 router.get("/:id",userAuth,getCandidateById)
 router.put('/:id',userAuth,upload.single('resume'), updateCandidate);
 router.delete('/:id', userAuth,deleteCandidate);
